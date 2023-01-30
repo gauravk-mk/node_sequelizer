be968534110fb8f953c7ae6cb14feb9ea0100fdc
Gaurav <gaurav@mortgagekart.com>
2023-01-30 17:24:58 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const { sequelize, User } = require('./models')
+
+
+const app= express()
+app.use(express.json())
+
+app.post('/users',async(req,res)=>{
+    const { name, email, role}  = req.body
+
+    try{
+        const user = await User.create({ name, email, role})
+        return res.json(user)
+
+    }catch(err){
+        console.log(err)
+        return res.status(500).json(err)
+    }
+})
+
+app.get('/users',async(req,res)=>{
+    try{
+        const users= await User.findAll()
+
+        return res.json(users)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({ error:'Something went wrong'})
+    }
+})
+
+app.listen({port: 5000}, async () => {
+    console.log('Server up on http://localhost:5000')
+    await sequelize.authenticate()
+    console.log("Database synced!")
+})
