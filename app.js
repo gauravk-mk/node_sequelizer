c9bb7de7b274bc0003c7419957c7b2f59f89ffde
Gaurav <gaurav@mortgagekart.com>
2023-01-31 15:58:58 +0530

    added nodemailer and nunjucks

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const { sequelize, User } = require('./models')
+const homeRoute = require('./routes/home')
+const sendEmails = require('./controllers/sendEmails')
+var path = require('path');
+let nunjucks = require('nunjucks')
 
 
 const app= express()
+
 app.use(express.json())
 
 app.post('/users',async(req,res)=>{
-    const { name, email, role}  = req.body
+    const { name, email, password}  = req.body
 
     try{
-        const user = await User.create({ name, email, role})
+        const user = await User.create({ name, email, password})
         return res.json(user)
 
     }catch(err){
@@ -28,6 +33,17 @@ app.get('/users',async(req,res)=>{
         return res.status(500).json({ error:'Something went wrong'})
     }
 })
+
+app.set("view engine", "html")
+
+nunjucks.configure(['datamodels/'], {   // setting a default views folder for templates 
+    autoescape: false,
+    express: app
+})
+
+
+app.use('/',homeRoute)
+app.use('/sendmail',sendEmails)
 
 app.listen({port: 5000}, async () => {
     console.log('Server up on http://localhost:5000')
--- migrations/20230130081304-create-user.js
+++ migrations/20230130081304-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -15,7 +15,7 @@ module.exports = {
       email: {
         type: Sequelize.STRING
       },
-      role: {
+      password: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
